cf72eef8c77b5f6bc562ceeb0b1e2b266f275826
DanyLugs <danylugo@ciencias.unam.mx>
2020-04-06 15:57:51 -0500

    Ver Categoria: agregado a views.py y urls.py

---
+++ Iphonda/comida/urls.py
@@ -0,0 +1,15 @@
+"""Comida URL configuration."""
+
+# Django
+from django.contrib import admin
+from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+# Views
+from comida import views
+
+app_name = "comida"
+urlpatterns = [
+    path('categorias', views-Categorias.as_view(), name = 'comida')
--- Iphonda/comida/views.py
+++ Iphonda/comida/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+"""Views de Comida."""
+from django.shortcuts import render, redircet
+from django.views import View
+from django.http import HttpResponse
+
+#Models
+from comida.models import Categoria
+
+#Forms
+from comida.forms import CategoryForm       #Cambiar por el nombre del form
 
 # Create your views here.
+class Categoria(View)
+
+    template = "comida/categorias/menu.html"
+
+    def get(self, request):
+        """GET method."""
+        categoria = Categoria.objects.all()
+        context = {"categoria": categoria}
+        return render(request, self.template, context)
