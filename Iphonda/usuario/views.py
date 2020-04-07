ea64b716a3e611fbeffb4c799946b383b24527df
LoboStark <rocadie9615@ciencias.unam.mx>
2020-04-07 12:15:30 -0500

    Vista de Los pedidos

--- Iphonda/usuario/views.py
+++ Iphonda/usuario/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from django.http import HttpResponse
 
 # Create your views here.
+
+class Pedidos(View):
+    """docstring forPedidos."""
+    template="usuario/pedidos.html"
+
+    def get(self,request):
+        context={}
+
+        return render(request,self.template,context)
+
+    def post(request):
+        return HttpResponse("<h1> no debiste llegar aqui </h1>")
+        pass
