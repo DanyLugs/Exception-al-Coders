120f181c6bf9acec470108c9f193bc762c24ed98
LoboStark <rocadie9615@ciencias.unam.mx>
2020-04-08 19:07:30 -0500

    No jalo la vista :,V

--- Iphonda/usuario/views.py
+++ Iphonda/usuario/views.py
@@ -2,19 +2,20 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.views import View
 from django.http import HttpResponse
-
+from .models import Orden
+from django.template import loader
 # Create your views here.
 
 class Pedidos(View):
     """docstring forPedidos."""
-    template="usuario/pedidos.html"
-
+    template=loader.get_template("usuario/pedidos.html")
+    lista_pedidos= Orden.objects.all()
     def get(self,request):
-        context={}
+        context={
+        'lista_pedidos':lista_pedidos,
+        }
 
-        return render(request,self.template,context)
-
+        return HttpResponse(template.render(context, request))
     def post(request):
         return HttpResponse("<h1> no debiste llegar aqui </h1>")
         pass
-##Comentario solo para subirlo agit
