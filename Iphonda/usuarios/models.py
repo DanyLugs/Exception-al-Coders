f70eb7417c7043a029b5662299ac692919477627
Juan Manuel Careaga <jmcareaga@ciencias.unam.mx>
2020-04-11 20:45:58 -0500

    Modelo comida.Orden relacionado con cliente

--- Iphonda/usuarios/models.py
+++ Iphonda/usuarios/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 # Create your models here.
 class Orden(models.Model):
     """docstring forOrden."""
-    id     = models.AutoField(primary_key=True)
-    # id_usuario  = models.ForeignKey('User.username', on_delete = models.PROTECT)
-    comida = models.ForeignKey('comida.Comida', on_delete = models.PROTECT)
-    fecha  = models.DateField()
-    total  = models.DecimalField(max_digits = 9, decimal_places = 2)
+    id      = models.AutoField(primary_key=True)
+    usuario = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.PROTECT)
+    comida  = models.ForeignKey('comida.Comida', on_delete = models.PROTECT)
+    fecha   = models.DateField()
+    total   = models.DecimalField(max_digits = 9, decimal_places = 2)
 
     def __str__(self):
         return self.id
